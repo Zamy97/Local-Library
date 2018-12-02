1ee4fe180320888af255b0d209e000d68e298006
Akhtar Zaman <auz.zamy@gmail.com>
2018-12-01 17:56:48 -0800

    onto bookinstace routes

--- routes/catalog.js
+++ routes/catalog.js
@@ -37,3 +37,58 @@ router.get('/book/:id', book_controller.book_detail)
 
 // GET request for list of all Book items.
 router.get('/books', book_controller.book_list)
+
+
+
+/// Author Routes //
+
+// GET request for creating Author
+
+route.get('/author/create', author_controller.author_create_get);
+
+// POST request for creating Author
+router.post('/author/create', author_controller.author_create_post)
+
+// GET request to delete author
+router.get('/author/:id/delete', author_controller.author_delete_get)
+
+// POST request to delete author
+router.post('/author/:id/delete', author_controller.author_delete_post)
+
+//GET request to update Author
+router.get('/author/:id/update', author_controller.author_update_get)
+
+//POST request to update Author
+router.post('/author/:id/update', author_controller.author_update_post)
+
+// GET request for one Author
+router.get('author/:id', author_controller.author_detail)
+
+// GET request for list of all authors
+router.get('/authos', author_controller.author_list)
+
+/// GENRE ROUTES ///
+
+// GET request for creating a Genre. NOTE This must come before route that displays Genre (uses id).
+router.get('/genre/create', genre_controller.genre_create_get);
+
+//POST request for creating Genre.
+router.post('/genre/create', genre_controller.genre_create_post);
+
+// GET request to delete Genre.
+router.get('/genre/:id/delete', genre_controller.genre_delete_get);
+
+// POST request to delete Genre.
+router.post('/genre/:id/delete', genre_controller.genre_delete_post);
+
+// GET request to update Genre.
+router.get('/genre/:id/update', genre_controller.genre_update_get);
+
+// POST request to update Genre.
+router.post('/genre/:id/update', genre_controller.genre_update_post);
+
+// GET request for one Genre.
+router.get('/genre/:id', genre_controller.genre_detail);
+
+// GET request for list of all Genre.
+router.get('/genres', genre_controller.genre_list);
